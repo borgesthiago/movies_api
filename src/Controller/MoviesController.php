20a0ba830a9482678cf26e45edddc27e2fc8c207
r.vasco83 <r.vasco83@hotmail.com>
2020-07-04 13:52:36 +0100

    Implementing Cache System

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -3,9 +3,12 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Finder\Finder;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use GuzzleHttp;
+use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
+use Symfony\Component\Filesystem\Filesystem;
 
 
 class MoviesController extends AbstractController
@@ -21,16 +24,36 @@ class MoviesController extends AbstractController
         $key = $_ENV['API_KEY'];
         $search = $request->get('s');
 
-        $client = new GuzzleHttp\Client();
-        $res = $client->request('GET', 'http://www.omdbapi.com/?apikey=' . $key . '/?s=' .$search, [
-            'headers' => [
-                'User-Agent' => 'testing/1.0',
-                'Accept' => 'application/json',
-            ]
-        ]);
+        $finder = new Finder();
+        $finder->files()->in('/var/www/public/tmp/')->name($search.'.json');
 
-        $resDecode = json_decode($res->getBody()->getContents());
+        if ($finder->hasResults()) {
+            //achou no cache
 
-        return $this->json([$resDecode]);
+            //pegar o resultado e enviar como json para tela
+
+
+            foreach ($finder as $file) {
+                $conteudo = json_encode($file->getContents(), JSON_PRETTY_PRINT);
+                dump(1);
+            }
+            return $this->json($conteudo);
+        } else {
+
+            $client = new GuzzleHttp\Client();
+            $res = $client->request('GET', 'http://www.omdbapi.com/?apikey=' . $key . '&s=' .$search,
+                [
+                    'headers'        => ['Accept-Encoding' => 'json'],
+                    'decode_content' => false
+                ]);
+
+            $resDecode = json_decode($res->getBody()->getContents(), true);
+
+            $filesystem = new Filesystem();
+            $content = $this->json($resDecode);
+            $filesystem->dumpFile('/var/www/public/tmp/' . $search . '.json', $content);
+
+            return $this->json($resDecode);
+        }
     }
 }
